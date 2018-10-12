8ab1882980ebac9473d0c5dc8000c6279e8de786
Alex Wilson <alex.wilson@joyent.com>
2018-10-11 17:16:05 -0700

    wip: attestation certs

--- schema/sdckey.js
+++ schema/sdckey.js
@@ -25,6 +25,15 @@ function SDCKey() {
             fingerprint: 1,
             pkcs: 1
         },
+        optional: {
+            attested: 1,
+            attestation: 0,  /* zero or more */
+
+            /* Specific to attested keys stored in Yubikeys. */
+            ykSerial: 1,
+            ykPinRequired: 1,
+            ykTouchRequired: 1
+        }
         strict: true
     });
 }
